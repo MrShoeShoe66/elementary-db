76025b84d521d70e9791680a83a9eaf1538c8137
MrShoe_ <mrshoeshoe66@gmail.com>
2022-10-02 22:07:19 -0700

    Update v3.1.1

--- edb.js
+++ edb.js
@@ -1,14 +1,14 @@
 module.exports              =       {}
-module.exports.version      =       'v3.1.0 - BETA'
+module.exports.version      =       'v3.1.1'
 module.exports.extenion     =       '.edb'
 module.exports.remote       =       {
     client: {
         offical: true,
-        version: "v1.1.0 - BETA"
+        version: "v1.1.0"
     },
     server: {
         offical: true,
-        version: "v1.1.0 - BETA"
+        version: "v1.1.0"
     },
     logs: "./edblogs.txt"
 }
